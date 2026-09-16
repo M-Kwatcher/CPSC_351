c7a47e5f22abc29c4ebdb6f4d9e0b4d4e4ac49e4
M-Kwatcher <mkwatcher2013@gmail.com>
2026-09-15 20:14:11 -0700

    added helper functions end_of_life_cleanup() and error_logic() to avoid repeating the same lines of code throughout the file

--- processes.cpp
+++ processes.cpp
@@ -1,14 +1,23 @@
 #include "processes.h"
 
 
+void end_of_life_cleanup(){
+	wait(NULL);
+	wait(NULL);
+	exit(0);
+}
+
+void error_logic(){
+	std::cout << "Error occurred during fork() call\n";
+	exit(-1);
+}
 
 void parent(){
 	pid_t pid;
 	pid = fork();
 
 	if (pid < 0){
-		std::cout << "Error occurred during fork() call\n";
-		exit(-1);
+		error_logic();
 	}
 	else if (pid == 0){
 		// call child1()
@@ -39,8 +48,7 @@ void child1(){
 	// Fork Child 2
 	pid_t pid1 = fork();
 	if (pid1 < 0) {
-		std::cout << "Error occurred during fork() call\n";
-		exit(-1);
+		error_logic();
 	}	
 	if (pid1 == 0) {
 		child2();
@@ -59,14 +67,40 @@ void child1(){
 	}
 
 	// child1 waits for both child processes to complete
-	wait(NULL);
-	wait(NULL);
-	exit(0);
+	end_of_life_cleanup();
 }
 
 void child2(){
 	std::cout << "I am child2 pid=" << getpid() << "; my parent is pid=" << getppid() << "\n";
-}
+	// need to call fork() twice 
+	// call exit(0) on child logic after invoking appropriate child() function
+	// call wait(NULL) twice before final exit(0) call below if logic
+/*
+	pid_t pid1 = fork();
+
+	if(pid1 < 0){
+		error_logic();
+	}
+
+	if(pid1 == 0){
+		// in child logic
+		child4();
+		exit(0);
+	}
+
+	pid_t pid2 = fork();
+
+	if(pid2 < 0){
+		error_logic();
+	}
+
+	if(pid2 == 0){
+		child5();
+		exit(0);
+	}
+	
+	end_of_life_cleanup();
+*/}
 
 void child3(){
 	std::cout << "I am child3 pid=" << getpid() << "; my parent is pid=" << getppid() << "\n";
