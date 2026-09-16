58afc871721d1d6b6b4da70ccf0477c74c48aaba
M-Kwatcher <mkwatcher2013@gmail.com>
2026-09-15 17:10:33 -0700

    commit before branching to test different if/else flow

--- processes.cpp
+++ processes.cpp
@@ -26,41 +26,24 @@ void parent(){
 }
 
 void child1(){
-	pid_t my_pid, parent_pid, f_pid;
+	pid_t my_pid, parent_pid, first_fork_pid, second_fork_pid;
 	my_pid = getpid();
 	parent_pid = getppid();
 
 	std::cout << "I am child pid=" << my_pid << "; my parent is pid=" << parent_pid << "\n";
 	
 	// now needs to call fork() to make child2 and child3
-	f_pid = fork();
+	first_fork_pid = fork();
 
-	if (f_pid < 0){
+	if (first_fork_pid < 0){
 		// in error logic
 		std::cout << "Error occurred during fork() call\n";
 		exit(-1);
 	}
-	else if(f_pid == 0){
-		// in child logic; call child2 and child3
+	else if(first_fork_pid == 0){
+		// in child logic; call child2
 		// I think I need to call fork() again here
-		f_pid = fork();
-		if (f_pid < 0){
-			std::cout << "Error occurred during fork() call\n";
-			exit(-1);
-		}
-		else if (f_pid == 0){
-			// in child logic; call child3
-			std::cout << "Calling child3\n";
-			child3();
-		}
-		else{
-			// in parent logic; call child2
-			std::cout << "Calling child2\n";
-			child2();
-			wait(NULL);
-			exit(0);
-		}
-		
+		child2();
 	}
 	else{
 		// in parent logic
@@ -68,6 +51,23 @@ void child1(){
 		wait(NULL);
 		exit(0);
 	}
+
+	second_fork_pid = fork();
+	if (second_fork_pid < 0){
+		// in error logic
+		std::cout << "Error occurred during fork() call\n";
+		exit(-1);		
+	}
+	else if(second_fork_pid == 0){
+		// in child logic
+		child3();
+	}
+	else{
+		// in parent logic
+		wait(NULL);
+		exit(0);
+	}
+
 }
 
 void child2(){
